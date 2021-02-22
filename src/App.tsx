51b7dedd85f54cf9932828ed1a9334e16d66d656
tranlinh <linh.cntt097@gmail.com>
2021-02-22 14:50:40 +0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="text-center text-red-600">
+            <h1>Hello World</h1>
+            <p>I'm linhtv97</p>
+        </div>
+    );
+}
+
+export default App;
